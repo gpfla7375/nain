8bb90b7b180109364fa3ecf213fc1f0f0c8aa910
maesiri <164867358+maesiri@users.noreply.github.com>
2024-06-05 19:26:01 +0900

    Update NoticeRepository.java

--- src/main/java/io/paioneer/nain/notice/jpa/repository/NoticeRepository.java
+++ src/main/java/io/paioneer/nain/notice/jpa/repository/NoticeRepository.java
@@ -8,14 +8,3 @@ public interface NoticeRepository extends JpaRepository<NoticeEntity, Integer> {
 
 
 }
-=======
-package io.paioneer.nain.notice.jpa.repository;
-
-import io.paioneer.nain.notice.jpa.entity.NoticeEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NoticeRepository extends JpaRepository<NoticeEntity, Integer> {
-
-
-}
->>>>>>> ef6239d6e78f36c17ac5809f35a9b1aa69dc937d
