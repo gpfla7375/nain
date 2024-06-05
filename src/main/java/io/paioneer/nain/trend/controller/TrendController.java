98277e927bded065878a3fd08eaa3e8e83311d32
DESKTOP-APA79D3\kimmi <kimminsik0726@gmail.com>
2024-06-05 14:17:36 +0900

    TrendController Commit and Push test

--- src/main/java/io/paioneer/nain/trend/controller/TrendController.java
+++ src/main/java/io/paioneer/nain/trend/controller/TrendController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/trend")
+@RequestMapping("/trends")
 @CrossOrigin
 public class TrendController {
 }
