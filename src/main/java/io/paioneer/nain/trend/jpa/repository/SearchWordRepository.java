8a2a21dd486dd1984ab386d4eb990a0e73e829e1
DESKTOP-APA79D3\kimmi <kimminsik0726@gmail.com>
2024-06-05 16:30:25 +0900

    Trend, SearchWord 파일 추가 AI 트렌드, 검색어 기능 추가

--- src/main/java/io/paioneer/nain/trend/jpa/repository/SearchWordRepository.java
+++ src/main/java/io/paioneer/nain/trend/jpa/repository/SearchWordRepository.java
@@ -1,9 +1,9 @@
 package io.paioneer.nain.trend.jpa.repository;
 
-import io.paioneer.nain.trend.jpa.entity.TrendEntity;
+import io.paioneer.nain.trend.jpa.entity.SearchWordEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TrendRepository extends JpaRepository<TrendEntity, Long>, TrendRepositoryCustom{
+public interface SearchWordRepository extends JpaRepository<SearchWordEntity, Long>, SearchWordRepositoryCustom{
 }
