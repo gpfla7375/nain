da5c2ef6f5adc7fd4ff416143d0ac8fd26c57f38
maesiri <164867358+maesiri@users.noreply.github.com>
2024-06-05 19:25:51 +0900

    Update NoticeQueryRepository.java
    Update NoticeQueryRepository.java

--- src/main/java/io/paioneer/nain/notice/jpa/repository/NoticeQueryRepository.java
+++ src/main/java/io/paioneer/nain/notice/jpa/repository/NoticeQueryRepository.java
@@ -22,28 +22,3 @@ public class NoticeQueryRepository {
 
 
 }
-=======
-package io.paioneer.nain.notice.jpa.repository;
-
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import io.paioneer.nain.notice.jpa.entity.QNoticeEntity;
-import jakarta.persistence.EntityManager;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
-
-@Repository
-@RequiredArgsConstructor
-public class NoticeQueryRepository {
-
-    private final JPAQueryFactory queryFactory;
-
-    private final EntityManager entityManager;
-
-    private QNoticeEntity notice = QNoticeEntity.noticeEntity;
-
-
-
-
-
-}
->>>>>>> ef6239d6e78f36c17ac5809f35a9b1aa69dc937d
