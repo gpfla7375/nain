056c7e29cd1f52ada942b72fb82ad73f9ff2c4b8
Jha <what1254@gmail.com>
2024-06-05 13:00:46 +0900

    CommunityEntity 컬럼 추가 작성

--- src/main/java/io/paioneer/nain/community/jpa/entity/CommunityEntity.java
+++ src/main/java/io/paioneer/nain/community/jpa/entity/CommunityEntity.java
@@ -1,4 +1,44 @@
 package io.paioneer.nain.community.jpa.entity;
 
+import io.paioneer.nain.member.jpa.entity.MemberEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Builder
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "TB_COMMUNITY_BOARD")
 public class CommunityEntity {
+    @Id
+    @Column(name="COMMUNITY_NO", nullable = false)
+    private Long communityNo;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="MEMBER_NO", insertable = false, updatable = false)
+    private MemberEntity memberEntity;
+
+    @Column(name="TITLE", nullable = false)
+    private String title;
+
+    @Column(name="COMMUNITY_CONTENT", nullable = false)
+    private String content;
+
+    @Column(name="FILE_UPLOAD", nullable = false)
+    private String filedUpload;
+
+    @Column(name="FILE_MODIFIED", nullable = false)
+    private String fileModified;
+
+    @Column(name="COMMUNITY_DATE")
+    private Date communityDate;
+
+    @Column(name="READCOUNT", nullable = false)
+    private String readCount;
 }
